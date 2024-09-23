70403c8cda8ace569005b333fc6bf6960f63f2c4
M.D.W.De Silva <wdulanjith@gmail.com>
2024-09-23 17:18:55 +0530

    jwt token generation code implemented

---
+++ src/main/java/com/eazybytes/constant/ApplicationConstants.java
@@ -0,0 +1,10 @@
+package com.eazybytes.constant;
+
+public final class ApplicationConstants {
+
+    public static final String JWT_SECRET_KEY = "JWT_SECRET";
+    public static final String JWT_SECRET_DEFAULT_VALUE = "aP98WZQtnYvH4vFiw3kM5th7XoFVF5DkNvAfNhtrPTM=";
+    public static final String JWT_HEADER = "Authorization";
+
+        private ApplicationConstants(){}
+}
